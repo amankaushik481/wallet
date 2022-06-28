01746e5660a3a58954be3d4f9d8efbbfff435a1c
Aman Kaushik <amankaushik481@gmail.com>
2022-06-29 00:56:58 +0530

    new wallet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing modules
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import "./App.css";
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", accountsChanged);
+      window.ethereum.on("chainChanged", chainChanged);
+    }
+  }, []);
+
+  const connectHandler = async () => {
+    if (window.ethereum) {
+      try {
+        const res = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        await accountsChanged(res[0]);
+        setWalletConnected(true);
+      } catch (err) {
+        console.error(err);
+        setErrorMessage("There was a problem connecting to MetaMask");
+      }
+    } else {
+      setErrorMessage("Install MetaMask");
+    }
+  };
+
+  const accountsChanged = async (newAccount) => {
+    setAccount(newAccount);
+    try {
+      const balance = await window.ethereum.request({
+        method: "eth_getBalance",
+        params: [newAccount.toString(), "latest"],
+      });
+      setBalance(ethers.utils.formatEther(balance));
+    } catch (err) {
+      console.error(err);
+      setErrorMessage("There was a problem connecting to MetaMask");
+    }
+  };
+
+  const chainChanged = () => {
+    connectHandler();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <strong>Address: </strong>
+      {account}
+      <br />
+      <strong>Balance: </strong>
+      {balance} {balance ? "ETH" : null}
+      {!walletConnected ? (
+        <button onClick={connectHandler} variant="primary">
+          Connect to wallet
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
